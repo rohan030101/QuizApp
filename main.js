a14d974d7d78711b2338bba3dd7b5ffbf609af75
rohan030101 <janraorohan2@gmail.com>
2025-03-09 22:27:20 +0530

    ui finalize moving to login and signup logic

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 console.log("Script Loaded");
 
-
-
-
 //To toggle the password visibilty
-let toggle = document.getElementById('togglePassword');
-let passwordFiled = document.getElementById('passwordFiled');
+let toggle = document.getElementById("togglePassword");
+let passwordFiled = document.getElementById("passwordFiled");
 
-toggle.addEventListener('click',()=>{
-    if(passwordFiled.type === "password"){
-        passwordFiled.type = "text";
-        toggle.classList.remove("fa-eye");
-        toggle.classList.add('fa-eye-slash');
-        console.log(passwordFiled.type);
-    }
-    else{
-        passwordFiled.type = "password";
-        toggle.classList.remove('fa-eye-slash');
-        toggle.classList.add('fa-eye');
-        console.log(passwordFiled.type);    
 
-    }
+toggle.addEventListener("click", () => {
+  if (passwordFiled.type === "password") {
+    passwordFiled.type = "text";
+    toggle.classList.remove("fa-eye");
+    toggle.classList.add("fa-eye-slash");
+    console.log(passwordFiled.type);
+  } else {
+    passwordFiled.type = "password";
+    toggle.classList.remove("fa-eye-slash");
+    toggle.classList.add("fa-eye");
+    console.log(passwordFiled.type);
+  }
+});
 
-})
+
+
+//Login and Signup Section
+
