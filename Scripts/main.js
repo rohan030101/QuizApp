b641a712ff84e7e360790ad693b23a3d3d545278
rohan030101 <janraorohan2@gmail.com>
2025-05-25 16:01:50 +0530

    Login Page and SignUp Page Done

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,7 @@
+if (window.location.pathname.endsWith("/pages/signup.html")) {
+  signup();
+}
+
 //To toggle the password visibilty
 let toggle = document.getElementById("togglePassword");
 let passwordFiled = document.getElementById("passwordField");
@@ -16,73 +20,98 @@ toggle.addEventListener("click", () => {
   }
 });
 
-
-function toQuizApp(){
-  addEventListener('click',()=>{
-    location.replace('/pages/quiz-page.html');  
+function toQuizApp() {
+  addEventListener("click", () => {
+    location.replace("/pages/quiz-page.html");
   });
 }
 
-
-
-/************* Login Page & Signup Page *************/ 
+/************* Login Page & Signup Page *************/
 
 const userName = document.getElementById("userName");
 const userEmail = document.getElementById("userEmail");
 const password = document.getElementById("passwordField");
 const singUpForm = document.getElementById("sign-up");
+const loginForm = document.getElementById("login");
 const errorEmail = document.getElementById("errorEmail");
 const errorName = document.getElementById("error-name");
+const erroPass = document.getElementById("error-pass");
 
-// document.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// });
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
+//if localstorage is empty
+// let dummyUser = {
+//     name: "Dummy",
+//     email: "Dummy@gmail.com",
+//     password: "Dummy@1234",
+//     id: new Date().toISOString(),
+//   };
+// localStorage.setItem('users', JSON.stringify([dummyUser]));
 
-//localstorage Declaration 
-let getLocalStorageUser = JSON.parse(localStorage.getItem('users'));
-
+//localstorage Declaration
+let getLocalStorageUser = JSON.parse(localStorage.getItem("users"));
 
 //Add user to localstorage:
-function addUser(fullName, userEmail, password){
-let newUser = {
-  name: fullName,
-  email:userEmail,
-  password:password,
-  id: new Date().toISOString()
+function addUser(fullName, userEmail, password) {
+  console.log(fullName);
+  console.log(userEmail);
+  console.log(password);
+  let newUser = {
+    name: fullName,
+    email: userEmail,
+    password: password,
+    id: new Date().toISOString(),
+  };
+  getLocalStorageUser.push(newUser);
+
+  localStorage.setItem("users", JSON.stringify(getLocalStorageUser));
+  console.log("user Added");
 }
-getLocalStorageUser.push(newUser);
 
-localStorage.setItem('users', JSON.stringify(getLocalStorageUser));
-console.log("user Added");
-
-} 
-
-
-//SignUp Page Validation
-singUpForm.addEventListener("submit", function (e) {
+// SignUp Page Validation and Adding user to localstorage
+singUpForm.addEventListener("submit", function signup(e) {
   const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const nameRegex = /^[A-Za-z]+$/;
+  const usernnameRegex = /^[A-Za-z]+(?: [A-Za-z]+)+$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 
-  if (!nameRegex.test(userName.value)) {
-
+  if (!usernnameRegex.test(userName.value)) {
     console.log("Error in name");
     errorName.innerHTML = "Please enter Valid name";
     e.preventDefault();
-  
   } else if (!emailregex.test(userEmail.value)) {
-  
     console.log("Error in email id");
     errorEmail.innerHTML = "Please enter valid email address!";
     e.preventDefault();
-  
+  } else if (!passwordRegex.test(password.value)) {
+    console.log(
+      "Password Should be  8 characters, Uppercase , Lowercase, Special Character and Numbers"
+    );
+    erroPass.innerHTML =
+      "Password Should be  8 characters, Uppercase , Lowercase, Special Character and Numbers";
+    e.preventDefault();
   } else {
-    console.log(addUser(userName,userEmail,password));
+    console.log(addUser(userName.value, userEmail.value, password.value));
     console.log("SignUp");
     alert("SignUp Successfully");
+    location.replace("/index.html");
   }
-
-  
 });
 
-/************* Login Page & Signup Page Ends Here *************/ 
+loginForm.addEventListener("submit", () => {
+  let users = JSON.parse(localStorage.getItem("users"));
+  let matchUser = users.find(
+    (user) => user.email === userEmail.value && user.password === password.value
+  );
+
+  if (matchUser != undefined) {
+    alert("Welcome" + userEmail.value);
+    location.replace("/pages/dashboard-page.html");
+  } else {
+    alert("Login ID Password Incorrect");
+    location.reload();
+  }
+});
+
+/************* Login Page & Signup Page Ends Here *************/
